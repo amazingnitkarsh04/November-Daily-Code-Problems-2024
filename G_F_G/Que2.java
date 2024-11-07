5dcda68f0b1a7bfebf30df7e16dc94b310b896f0
Nitkarsh Mishra <123670630+amazingnitkarsh04@users.noreply.github.com>
2024-11-07 23:58:35 +0530

    Add files via upload

---
+++ G_F_G/Que2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+//Take name as input and print a greeting message for that particular name.
+public class Que2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Provise any massage: ");
+
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
